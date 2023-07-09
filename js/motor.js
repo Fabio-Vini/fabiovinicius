632ce1b813691731d92b768fcd65981892344535
Fabio-Vini <88862357+Fabio-Vini@users.noreply.github.com>
2023-07-09 14:32:41 -0300

    Add files via upload

---
+++ js/motor.js
@@ -0,0 +1,66 @@
+//-------SCRIPT DO MENU RESPONSIVO----------//
+
+const nav = document.querySelector(".nav");
+const btnMenu = document.querySelector(".btn-menu");
+const menu = document.querySelector(".menu");
+
+function handleButtonClick(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  event.stopPropagation();
+  nav.classList.toggle("active");
+  handleClickOutside(menu, () => {
+    nav.classList.remove("active");
+    setAria();
+  });
+  setAria();
+}
+
+function handleClickOutside(targetElement, callback) {
+  const html = document.documentElement;
+  function handleHTMLClick(event) {
+    if (!targetElement.contains(event.target)) {
+      targetElement.removeAttribute("data-target");
+      html.removeEventListener("click", handleHTMLClick);
+      html.removeEventListener("touchstart", handleHTMLClick);
+      callback();
+    }
+  }
+  if (!targetElement.hasAttribute("data-target")) {
+    html.addEventListener("click", handleHTMLClick);
+    html.addEventListener("touchstart", handleHTMLClick);
+    targetElement.setAttribute("data-target", "");
+  }
+}
+
+function setAria() {
+  const isActive = nav.classList.contains("active");
+  btnMenu.setAttribute("aria-expanded", isActive);
+  if (isActive) {
+    btnMenu.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    btnMenu.setAttribute("aria-label", "Abrir Menu");
+  }
+}
+
+btnMenu.addEventListener("click", handleButtonClick);
+btnMenu.addEventListener("touchstart", handleButtonClick);
+
+
+
+
+//--------SCRIPT DA ANIMAÇÃO DA DIV ".sobre-mim"----------//
+
+function slideRightOnScroll() {
+    var elements = document.querySelectorAll('.sobre-mim');
+
+    Array.prototype.forEach.call(elements, function(element) {
+      var elementPosition = element.getBoundingClientRect().top;
+      var screenPosition = window.innerHeight;
+
+      if (elementPosition < screenPosition) {
+        element.classList.add('animate');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', slideRightOnScroll);
